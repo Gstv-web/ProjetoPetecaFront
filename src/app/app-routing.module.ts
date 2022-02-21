a7b7d140e53beb704b27b6e1e45dfa5c21fddcac
Paulo <pmarsiglia93@gmail.com>
2022-02-21 17:35:03 -0300

    @pmarsiglia93 [TASK: ARRUMANDO AS ROTAS]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+
 
 const routes: Routes = [
   {path:'', redirectTo:'entrar', pathMatch:'full'},
 
   //Rotas
   
+  {path:'entrar', component: EntrarComponent},
   {path:'cadastrar', component: CadastrarComponent},
 
 ];
