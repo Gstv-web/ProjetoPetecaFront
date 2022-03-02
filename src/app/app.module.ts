33ba29acf9bc863e11f1bcdde4172f9340301c30
Paulo <pmarsiglia93@gmail.com>
2022-03-02 20:13:56 -0300

    @pmarsiglia93 [TASK: ARRUMANDO TELA INICIAL]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,16 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { MenuComponent } from './menu/menu.component';
-import { EntrarComponent } from './entrar/entrar.component';
-import { RodapeComponent } from './rodape/rodape.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { ContatoComponent } from './contato/contato.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-
-
 
 
 @NgModule({
