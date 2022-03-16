c0c60a0b93ac2c024d1c6d2aa3f03adcee52a7a3
Jessica Curti <jessica.m.curti@gmail.com>
2022-03-16 20:25:33 -0300

    @Jess-Curti[Ajustes]

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -3,7 +3,6 @@ import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 import { Credentials } from "../model/Credentials";
 import { AuthService } from '../service/auth.service';
-import { User } from '../model/User';
 import { AlertasService } from '../service/alertas.service';
 
 
@@ -15,9 +14,6 @@ import { AlertasService } from '../service/alertas.service';
 export class EntrarComponent implements OnInit {
 
   credentials: Credentials = new Credentials()
-  user: User = new User
-  confirmarSenha: string
-  tipo: string
 
   constructor(
     private auth: AuthService,
@@ -43,32 +39,10 @@ export class EntrarComponent implements OnInit {
       },
       error: erro => {
       if (erro.status == 400) {
-        alert("Usuário ou senha inválidos");
+        this.alertas.showAlertDanger("Usuário ou senha inválidos");
         }
       }
     });
-  }
-
-  confirmSenha(event: any) {
-    this.confirmarSenha = event.target.value
-  }
-
-  tipoUser(event: any) {
-    this.tipo = event.target.value
-  }
-
-  cadastrar() {
-    this.user.tipo = this.tipo
-
-    if(this.user.senha != this.confirmarSenha){
-      alert("A senha está incorreta.")
-    } else {
-      this.auth.cadastrar(this.user).subscribe((resp: User) => {
-        this.user = resp
-        this.alertas.showAlertSuccess('Usuário cadastrado com sucesso!')
-        this.router.navigate(["/entrar"])
-      })
-    }
   }
 
 }
